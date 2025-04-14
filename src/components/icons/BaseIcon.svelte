78923a1bf723bc3cba4d76a7f3c4efd7b9b56acd
Scott C. Gilmore <scott@scottgilmore.dev>
2025-04-14 18:37:15 -0400

    Fix typo

--- src/components/icons/BaseIcon.svelte
+++ src/components/icons/BaseIcon.svelte
@@ -5,23 +5,23 @@
    * @component
    * @description A base component for rendering SVG icons using path data from the icon registry.
    * This component handles the common SVG structure and attributes, while the specific path
-   * data is retrieved from the iconPaths registry based on the provided name.
+   * data is retrieved from the ICON_PATHS registry based on the provided name.
    *
-   * @prop {keyof typeof iconPaths} name - The name of the icon to render from the iconPaths registry
+   * @prop {keyof typeof ICON_PATHS} name - The name of the icon to render from the ICON_PATHS registry
    * @prop {string} [width="24"] - Width of the icon in pixels
    * @prop {string} [height="24"] - Height of the icon in pixels
    * @prop {string} [className=""] - Additional CSS classes to apply to the SVG
    *
-   * @requires module:@types
+   * @requires module:@constants
    */
 
-  import { iconPaths } from '@types';
+  import { ICON_PATHS } from '@constants';
 
   /**
    * Props for the BaseIcon component
    *
    * @interface BaseIconProps
-   * @property {keyof typeof iconPaths} name - The name of the icon to render
+   * @property {keyof typeof ICON_PATHS} name - The name of the icon to render
    * @property {string} [width="24"] - Width of the icon in pixels
    * @property {string} [height="24"] - Height of the icon in pixels
    * @property {string} [className=""] - Additional CSS classes to apply to the SVG
@@ -29,7 +29,7 @@
   interface BaseIconProps {
     className?: string;
     height?: string;
-    name: keyof typeof iconPaths;
+    name: keyof typeof ICON_PATHS;
     width?: string;
   }
 
@@ -38,8 +38,8 @@
   export let name: BaseIconProps['name'];
   export let width: BaseIconProps['width'] = '24';
 
-  $: path = iconPaths[name]?.path || '';
-  $: viewBox = iconPaths[name]?.viewBox || '0 0 24 24';
+  $: path = ICON_PATHS[name]?.path || '';
+  $: viewBox = ICON_PATHS[name]?.viewBox || '0 0 24 24';
 </script>
 
 <svg
